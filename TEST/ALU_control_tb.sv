46658c896f95e5ab2f8e037fd1028dc2e0a80c04
lijyhh <1285507636@qq.com>
2022-04-03 14:29:08 +0800

    Test ALU control

--- TEST/ALU_control_tb.sv
+++ TEST/ALU_control_tb.sv
@@ -30,8 +30,6 @@ module ALU_control_tb();
   reg  [01:0]               tb_ALUOp ;                
   reg  [03:0]               tb_ALUCtl; 
 
-  reg  [03:0]               tb_cnt   ;
-
   ALU_control ALU_control_TB(
   .opcode( tb_opcode )  ,    // Opcode of instruction             
   .ALUOp ( tb_ALUOp  )  ,    // ALUOp from control unit     
@@ -49,35 +47,30 @@ module ALU_control_tb();
 
     tb_opcode = 11'bxxxxxxxxxxx;
     tb_ALUOp  = 2'b01;
-    tb_cnt    = tb_cnt + 1'b1;
     #`CYCLE;
     assert( tb_ALUCtl == 4'b0111 ) $strobe("%0d, !!TEST SUCCESS!!", $time);
     else $error("[1] tb_ALUCtl = %4b", tb_ALUCtl);
 
     tb_opcode = 11'b10001011000;
     tb_ALUOp  = 2'b10;
-    tb_cnt    = tb_cnt + 1'b1;
     #`CYCLE;
     assert( tb_ALUCtl == 4'b0010 ) $strobe("%0d, !!TEST SUCCESS!!", $time);
     else $error("[2] tb_ALUCtl = %4b", tb_ALUCtl);
 
     tb_opcode = 11'b11001011000;
     tb_ALUOp  = 2'b10;
-    tb_cnt    = tb_cnt + 1'b1;
     #`CYCLE;
     assert( tb_ALUCtl == 4'b0110 ) $strobe("%0d, !!TEST SUCCESS!!", $time);
     else $error("[3] tb_ALUCtl = %4b", tb_ALUCtl);
 
     tb_opcode = 11'b10001010000;
     tb_ALUOp  = 2'b10;
-    tb_cnt    = tb_cnt + 1'b1;
     #`CYCLE;
     assert( tb_ALUCtl == 4'b0000 ) $strobe("%0d, !!TEST SUCCESS!!", $time);
     else $error("[4] tb_ALUCtl = %4b", tb_ALUCtl);
 
     tb_opcode = 11'b10101010000;
     tb_ALUOp  = 2'b10;
-    tb_cnt    = tb_cnt + 1'b1;
     #`CYCLE;
     assert( tb_ALUCtl == 4'b0001 ) $strobe("%0d, !!TEST SUCCESS!!", $time);
     else $error("[5] tb_ALUCtl = %4b", tb_ALUCtl);
